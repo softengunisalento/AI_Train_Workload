eacab58cb21dd60115eb3bcd960c51d8a2c970eb
alessioerrico <alessio.errico1@gmail.com>
2023-05-30 21:13:03 +0200

    env var config push

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import workload
 import sys
 
 if __name__ == '__main__':
+
     selected_workload = sys.argv[1]
     workload_algorithm = workload.Workload(measure_power_secs=15)
     workload_algorithm.compute_workload_consumption(selected_workload)
