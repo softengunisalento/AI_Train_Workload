d372043c4bd1e7b3ce5a557a60be18cf93650d8d
alessioerrico <alessio.errico1@gmail.com>
2023-05-31 23:23:31 +0200

    TRY TO FIX writing csv

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import os
+
 import workload
 import sys
 
 if __name__ == '__main__':
-    f = open("demofile3.txt", "w")
-    f.write("Woops! I have deleted the content!")
-    f.close()
+    print("CURRENT DIR:", os.getcwd())
     # selected_workload = sys.argv[1]
     # workload_algorithm = workload.Workload(measure_power_secs=15)
     # workload_algorithm.compute_workload_consumption(selected_workload)
