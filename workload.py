ce29b6137b92d2f7b6e336fc1ae6909eb9b42c4d
alessioerrico <alessio.errico1@gmail.com>
2023-06-02 15:48:52 +0200

    TRY TO FIX writing csv

--- workload.py
+++ workload.py
@@ -56,7 +56,7 @@ class Workload:
     def grid_search_isolation_forrest(self):
         print("GRID-SEARCH-ISOLATION FORREST IS STARTING")
         estimators = [20, 40, 60]
-        max_samples = [100]#[100, 200, 400]
+        max_samples = [100, 200, 400]
         contamination = [0.1]#[0.1, 0.01, 0.001]
         random_states = [42]#[42, 62, 82]
         verbose = [8]#[8, 16, 32]
